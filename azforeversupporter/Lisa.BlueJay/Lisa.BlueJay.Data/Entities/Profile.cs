f5062a7e2342c2ff9809336587aedde384fb00fa
Robert <azforeversupporter@gmail.com>
2014-02-12 22:00:53 +0100

    Added entities for the database in project Lisa.BlueJay.Data. Still a subject to change.

---
+++ azforeversupporter/Lisa.BlueJay/Lisa.BlueJay.Data/Entities/Profile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lisa.BlueJay.Data.Entities
+{
+    public class Profile
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int DossierId { get; set; }
+
+        public virtual Dossier Dossier { get; set; }
+    }
+}
---
+++ azforeversupporter/Lisa.BlueJay/Lisa.BlueJay.Data/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Lisa.BlueJay.Data.Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DefaultValue("PARENT")]
+        public string Type { get; set; }
+
+        public virtual ICollection<Dossier> Dossiers { get; set; }
+    }
+}
